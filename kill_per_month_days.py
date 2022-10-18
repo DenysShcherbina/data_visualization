5c4d84765211f3fafdc83a3b7b3dd336b26a008c
Denys Shcherbina <114299504+DenysShcherbina@users.noreply.github.com>
2022-10-18 16:15:01 +0200

    Create kill_per_month_days.py

---
+++ kill_per_month_days.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Create DataFrame from CSV
+shootings = pd.read_csv('shootings.csv')
+
+# Convert type of data in column
+shootings['date'] = pd.to_datetime(shootings['date'])
+
+# Create new columns
+shootings['month'] = shootings['date'].apply(lambda x: x.month)
+shootings['day'] = shootings['date'].apply(lambda x: x.day)
+
+# Create pivot table for heatmap
+month_day = shootings.pivot_table(values=shootings, index='day', columns='month', aggfunc='count')['age']
+
+plt.figure(figsize=(10, 6))
+sns.heatmap(data=month_day, cmap='plasma')
+plt.show()
